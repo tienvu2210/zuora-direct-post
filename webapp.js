bea1e3c6a035cdc7434d69121ecc6c8fac3671be
Christopher Thilgen <christopherthilgen@asana.com>
2021-08-02 18:41:22 -0700

    replace inline html with standalone index.html file

--- webapp.js
+++ webapp.js
@@ -1,15 +1,17 @@
 const http = require('http')
 const url = require('url')
+const fs = require('fs')
 
 const onRequest = async(request, response) => {
     let pathName = url.parse(request.url).pathname
     console.log('onRequest:pathname: ' + pathName);
 
     switch (pathName) {
-        case '/':
+        case '/' :
             // draw the main page with invoices and hpm
+            let indexHtml = fs.readFileSync('./pages/index.html', 'utf8')
             response.writeHead(200, {'Content-Type': 'text/html'})
-            response.write('<h1>Hello!</h1>');
+            response.write(indexHtml);
             response.end();
             break;
         default:
