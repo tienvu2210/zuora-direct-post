a7b550f534095d20f512f30e9b3dc8e2f97003db
Christopher Thilgen <christopherthilgen@asana.com>
2022-02-07 20:47:19 -0800

    cleaned up hom parms generation; adding logging of payment id

--- api/hpmParams.js
+++ api/hpmParams.js
@@ -5,6 +5,8 @@ const auth = require('../util/auth');
 const zuora = require('../util/zuoraEndpoints');
 
 const getHpmParams = async (hpmPageId) => {
+    let zuoraAccountId = process.env.ZUORA_ACCOUNT_ID
+
     let authTokenResponse = await auth.getAuthToken();
     let access_token = authTokenResponse.data.access_token;
 
@@ -14,7 +16,8 @@ const getHpmParams = async (hpmPageId) => {
       data: {
         method: 'POST',
         uri: zuora.HPM_URI,
-        pageId: hpmPageId
+        pageId: hpmPageId,
+        accountId: zuoraAccountId
       },
       headers: {
         'content-Type': 'application/json',
@@ -22,7 +25,6 @@ const getHpmParams = async (hpmPageId) => {
       }
     });
 
-    let zuoraAccountId = process.env.ZUORA_ACCOUNT_ID
     let zuoraGatewayName = process.env.ZUORA_GATEWAY_NAME
 
     hpmParams = {}
@@ -31,13 +33,17 @@ const getHpmParams = async (hpmPageId) => {
     hpmParams.tenantId = response.data.tenantId
     hpmParams.key = response.data.key
     hpmParams.id = hpmPageId
-    hpmParams.field_accountId = zuoraAccountId
+    hpmParams.field_accountId = zuoraAccountId // must match the value passed into /v1/rsa-signatures endpoint
     hpmParams.paymentGateway = zuoraGatewayName
     hpmParams.url = zuora.HPM_URI
     hpmParams.style = 'inline'
     hpmParams.submitEnabled = 'true'
     hpmParams.locale = 'en'
     hpmParams.param_supportedTypes = 'Visa,MasterCard,Discover'
+
+    hpmParams.doPayment = true
+    hpmParams.storePaymentMethod = false
+    hpmParams.documents = "[{\"type\": \"invoice\", \"ref\": \"INV00234983\"}]"
 
     return hpmParams
 }
